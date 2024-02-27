5b11776a4e83656e98dd286bd2c0248471aa7081
Usman Ashfaq <120087782+Usman-Ashfi@users.noreply.github.com>
2024-02-27 11:50:38 +0500

    useEffect

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 const page = () => {
   let [count, setCount] = useState(false);
+  let [count1, setCount1] = useState(false);
+
+  useEffect(() => {
+    console.log("hi");
+  }, [count]);
 
   function chg() {
     if (count) {
@@ -12,10 +17,25 @@ const page = () => {
       setCount(true);
     }
   }
+
+  function chgs() {
+    if (count1) {
+      setCount1(false);
+    } else {
+      setCount1(true);
+    }
+  }
   return (
     <div className="flex flex-col justify-center items-center py-10 px-4 ">
-      <div>{count ? <h1>Good Work</h1> : <></>}</div>
-      <button onClick={() => chg()}>Click Me</button>
+      <div className="border py-6 px-4">
+        <div>{count ? <h1>Good Work</h1> : <h1>dependcey</h1>}</div>
+        <button onClick={() => chg()}>Click Me</button>
+      </div>
+
+      <div className="border py-6 px-4">
+        <div>{count1 ? <h1>Good Work</h1> : <h1>Count1</h1>}</div>
+        <button onClick={() => chgs()}>Click Me</button>
+      </div>
     </div>
   );
 };
