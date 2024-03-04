838c81cc2069c1a46828410f88c06eb4ee914f83
Usman Ashfaq <120087782+Usman-Ashfi@users.noreply.github.com>
2024-03-04 14:09:02 +0500

    form data entries

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,43 +1,18 @@
 "use client";
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Loading from "@/components/Loading";
+import Hero from "@/components/Hero";
 
 const page = () => {
-  let [count, setCount] = useState(false);
-  let [count1, setCount1] = useState(false);
+  const [loader, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log("hi");
-  }, [count]);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
-  function chg() {
-    if (count) {
-      setCount(false);
-    } else {
-      setCount(true);
-    }
-  }
-
-  function chgs() {
-    if (count1) {
-      setCount1(false);
-    } else {
-      setCount1(true);
-    }
-  }
-  return (
-    <div className="flex flex-col justify-center items-center py-10 px-4 ">
-      <div className="border py-6 px-4">
-        <div>{count ? <h1>Good Work</h1> : <h1>dependcey</h1>}</div>
-        <button onClick={() => chg()}>Click Me</button>
-      </div>
-
-      <div className="border py-6 px-4">
-        <div>{count1 ? <h1>Good Work</h1> : <h1>Count1</h1>}</div>
-        <button onClick={() => chgs()}>Click Me</button>
-      </div>
-    </div>
-  );
+  return <div>{loader ? <Loading /> : <Hero />}</div>;
 };
 
 export default page;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div class="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
